371f84291ac02ad231a5bc505e2f8a33eac223a2
jjingofarouk <jjingofaroukk@gmail.com>
2025-04-02 21:08:22 +0300

    Update social-media-icons.tsx

--- src/components/social/social-media-icons.tsx
+++ src/components/social/social-media-icons.tsx
@@ -4,7 +4,6 @@ import { useInView } from "framer-motion";
 import React, { useRef } from "react";
 import { Button } from "../ui/button";
 import { SiGithub, SiInstagram, SiLinkedin, SiTwitter } from "react-icons/si";
-import { Linkedin } from "lucide-react";
 import { config } from "@/data/config";
 import Link from "next/link";
 
@@ -12,34 +11,35 @@ const BUTTONS = [
   {
     name: "Github",
     href: config.social.github,
-    icon: <SiGithub size={"24"} color={"#fff"} />,
+    icon: <SiGithub size={24} color="#fff" />,
   },
   {
     name: "LinkedIn",
     href: config.social.linkedin,
-    icon: <SiLinkedin size={"24"} color={"#fff"} />,
+    icon: <SiLinkedin size={24} color="#fff" />,
   },
   {
     name: "Twitter",
     href: config.social.twitter,
-    icon: <SiTwitter size={"24"} color={"#fff"} />,
+    icon: <SiTwitter size={24} color="#fff" />,
   },
   {
     name: "Instagram",
     href: config.social.instagram,
-    icon: <SiInstagram size={"24"} color={"#fff"} />,
+    icon: <SiInstagram size={24} color="#fff" />,
   },
 ];
 
-const SocialMediaButtons = () => {
+const SocialMediaButtons = ({ className }: { className?: string }) => {
   const ref = useRef<HTMLDivElement>(null);
   const show = useInView(ref, { once: true });
+
   return (
-    <div ref={ref} className="z-10">
+    <div ref={ref} className={`z-10 ${className || ""}`}>
       {show &&
         BUTTONS.map((button) => (
           <Link href={button.href} key={button.name} target="_blank">
-            <Button variant={"ghost"}>{button.icon}</Button>
+            <Button variant="ghost">{button.icon}</Button>
           </Link>
         ))}
     </div>
