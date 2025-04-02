e2935840d6987f6e39623ed9abbd474a2a76e434
jjingofarouk <jjingofaroukk@gmail.com>
2025-04-02 21:13:36 +0300

    Update use-mouse-position.ts

--- src/hooks/use-mouse-position.ts
+++ src/hooks/use-mouse-position.ts
@@ -1,15 +1,21 @@
 import React, { useEffect, useState } from "react";
 
-export const useMousePosition = () => {
+export const useMousePosition = ({ smoothFactor = 1 } = {}) => {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
   useEffect(() => {
     const handleMouseMove = (event: MouseEvent) => {
-      setMousePosition({ x: event.clientX, y: event.clientY });
+      setMousePosition((prev) => ({
+        x: prev.x + (event.clientX - prev.x) * smoothFactor,
+        y: prev.y + (event.clientY - prev.y) * smoothFactor,
+      }));
     };
+
     window.addEventListener("mousemove", handleMouseMove);
     return () => {
       window.removeEventListener("mousemove", handleMouseMove);
     };
-  }, []);
+  }, [smoothFactor]);
+
   return mousePosition;
 };
