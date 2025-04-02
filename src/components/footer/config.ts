014e0d9a4f328b8090afd9ad8b00cc48c8e2e42a
jjingofarouk <jjingofaroukk@gmail.com>
2025-04-02 20:35:02 +0300

    Update config.ts

--- src/components/footer/config.ts
+++ src/components/footer/config.ts
@@ -1,11 +1,18 @@
-const footer: { title: string; href: string }[] = [
+export const footer: { title: string; href: string; description?: string }[] = [
   {
     title: "Blog",
     href: "/blogs",
+    description: "Read my latest thoughts on tech & dev",
   },
   {
     title: "Newsletter",
     href: "/news",
+    description: "Stay updated with my coding journey",
+  },
+  {
+    title: "Projects",
+    href: "/projects",
+    description: "Explore my latest creations",
   },
 ];
 
