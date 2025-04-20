deaec2d527c12f03d14e7bbcd689dfec678193db
jjingofarouk <jjingofaroukk@gmail.com>
2025-04-20 15:45:20 +0300

    Update types.ts

--- src/data/projects/types.ts
+++ src/data/projects/types.ts
@@ -1,21 +1,22 @@
 // src/data/projects/types.ts
-import { ReactNode } from 'react';
-
-export type Skill = {
+export interface Skill {
   title: string;
   bg: string;
   fg: string;
-  icon: ReactNode;
-};
+  icon: React.ReactNode;
+}
 
-export type Project = {
+export interface Project {
   id: string;
   category: string;
   title: string;
   src: string;
   screenshots: string[];
-  skills: { frontend: Skill[]; backend: Skill[] };
-  content: ReactNode;
-  github?: string;
   live: string;
-};
+  github: string;
+  skills: {
+    frontend: Skill[];
+    backend: Skill[];
+  };
+  content: React.ReactNode;
+}
