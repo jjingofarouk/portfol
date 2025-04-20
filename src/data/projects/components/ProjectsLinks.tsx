df3fdb73e83472ad9aa0f4d205ab2eea7ae98230
jjingofarouk <jjingofaroukk@gmail.com>
2025-04-20 15:08:52 +0000

    hi

--- src/data/projects/components/ProjectsLinks.tsx
+++ src/data/projects/components/ProjectsLinks.tsx
@@ -1,15 +1,15 @@
-// src/data/projects/components/ProjectLinks.tsx
+// src/data/projects/components/ProjectsLinks.tsx
 import { Button } from '@/components/ui/button';
 import { ArrowUpRight } from 'lucide-react';
 import Link from 'next/link';
 import { FC } from 'react';
 
-interface ProjectLinksProps {
+interface ProjectsLinksProps {
   live: string;
   repo?: string;
 }
 
-export const ProjectLinks: FC<ProjectLinksProps> = ({ live, repo }) => (
+export const ProjectsLinks: FC<ProjectsLinksProps> = ({ live, repo }) => (
   <div className="flex flex-col md:flex-row items-center justify-start gap-3 my-3 mb-8">
     <Link className="font-mono underline flex gap-2" rel="noopener" target="_new" href={live}>
       <Button variant="default" size="sm">
