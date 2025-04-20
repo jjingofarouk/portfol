841f37170f8ea5965f3117d4ddfc2e6205913296
jjingofarouk <jjingofaroukk@gmail.com>
2025-04-20 17:32:01 +0300

    Update index.ts

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,4 +1,4 @@
 export { default as projects } from './projects/projects';
-export type { Project, Skill } from '@/projects/types'; // Change - to @
-export { PROJECT_SKILLS } from '@/projects/skills'; // Change - to @
-export { ProjectsLinks } from '@/projects/components/ProjectsLinks.tsx'; // Change - to @
+export type { Project, Skill } from './projects/types';
+export { PROJECT_SKILLS } from './projects/skills';
+export { ProjectsLinks } from './projects/components/ProjectsLinks.tsx';
