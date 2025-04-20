be989a16e8e66c9f526e360a1eeec95247abf2fe
jjingofarouk <jjingofaroukk@gmail.com>
2025-04-20 14:01:00 +0300

    Update index.ts

---
+++ src/data/projects/index.ts
@@ -0,0 +1,5 @@
+// src/data/projects/index.ts
+export { default as projects } from './projects';
+export type { Project, Skill } from './types';
+export { PROJECT_SKILLS } from './skills';
+export { ProjectsLinks } from './components/ProjectsLinks';
