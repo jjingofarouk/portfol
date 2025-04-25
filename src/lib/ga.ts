78dd396ee2794011d9bef75cabf3ee3cb53b9649
jjingofarouk <jjingofaroukk@gmail.com>
2025-04-26 00:06:01 +0300

    Delete src/lib/ga.ts

--- src/lib/ga.ts
+++
@@ -1,8 +0,0 @@
-// lib/ga.ts
-export const GA_MEASUREMENT_ID = 'G-VV2EQ7JH2R'; // Your actual GA ID
-
-export const pageview = (url: string) => {
-  window.gtag('config', GA_MEASUREMENT_ID, {
-    page_path: url,
-  });
-};
