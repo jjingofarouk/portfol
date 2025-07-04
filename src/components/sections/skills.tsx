0b9ae0db16e29622674245e3a3839f6ed11c73ef
jjingofarouk <jjingofaroukk@gmail.com>
2025-07-04 19:13:00 +0300

    Update skills.tsx

--- src/components/sections/skills.tsx
+++ src/components/sections/skills.tsx
@@ -2,7 +2,6 @@ import Link from "next/link";
 import React from "react";
 import { BoxReveal } from "../reveal-animations";
 import { cn } from "@/lib/utils";
-import GitHubCalendar from "react-github-calendar";
 
 const SkillsSection = () => {
   return (
@@ -14,7 +13,7 @@ const SkillsSection = () => {
               className={cn(
                 "bg-clip-text text-4xl text-center text-transparent md:text-7xl",
                 "bg-gradient-to-b from-black/80 to-black/50",
-                "dark:bg-gradient-to-b dark:from-white/80 dark:to-white/20 dark:bg-opacity-50"
+                "dark:bg-gradient-to-b dark:from-white/80 dark:to-white/20 dark:bg-opacity-50 "
               )}
             >
               SKILLS
@@ -22,22 +21,8 @@ const SkillsSection = () => {
           </BoxReveal>
         </Link>
         <p className="mx-auto mt-4 line-clamp-4 max-w-3xl font-normal text-base text-center text-neutral-300">
-          My coding journey, visualized through GitHub contributions
+          (hint: press a key)
         </p>
-        <BoxReveal width="100%">
-          <div className="mt-8 flex justify-center">
-            <GitHubCalendar
-              username="jjingofarouk" // Replace with your GitHub username
-              blockSize={15}
-              blockMargin={5}
-              fontSize={16}
-              theme={{
-                light: ["#ebedf0", "#9be9a8", "#40c463", "#30a14e", "#216e39"],
-                dark: ["#1a1e22", "#9be9a8", "#40c463", "#30a14e", "#216e39"],
-              }}
-            />
-          </div>
-        </BoxReveal>
       </div>
     </section>
   );
